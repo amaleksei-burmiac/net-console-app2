c3d717c8ec17bc04b5d203b349aa6f0e85bd71ea
amaleksei-burmiac <test@gmail.com>
2023-09-11 10:49:30 +0800

    Добавлена проверка числа на четность

---
+++ ConsoleApp1/CheckIsEvenNumber.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1
+{
+    internal class CheckIsEvenNumber
+    {
+        public bool IsEven(int number) { 
+            return number % 2 == 0;
+        }
+
+        public string OutputCheckMessage(int number)
+        {
+            bool isEven = IsEven(number);
+            return isEven ? $"число {number} - четное" : $"число {number} - нечетное";
+        }
+
+        public void CheckEvenNumber()
+        {
+            Console.WriteLine("Введите число для проверки на четность: ");
+            string? userInput = Console.ReadLine();
+
+            if (userInput != null)
+            {
+                int userInputNumber = Convert.ToInt32(userInput);
+                string result = OutputCheckMessage(userInputNumber);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
 
 // Суммирование массива чисел
 int[] numbers = new int[] { 1, 10, 20, 30 };
 int sum = numbers.Sum();
 Console.WriteLine("Сумма чисел: " +  sum);
+
+CheckIsEvenNumber checkIsEvenNumber = new CheckIsEvenNumber();
+checkIsEvenNumber.CheckEvenNumber();
