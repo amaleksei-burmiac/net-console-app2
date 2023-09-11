e11722cd6c13143e7fb537a75c3622ad57b40ac5
amaleksei-burmiac <test@gmail.com>
2023-09-11 10:15:44 +0800

    Добавлено суммирование массива чисел

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Суммирование массива чисел
+int[] numbers = new int[] { 1, 10, 20, 30 };
+int sum = numbers.Sum();
+Console.WriteLine("Сумма чисел: " +  sum);
